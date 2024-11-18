72dbd00c3c5a8c21174956992b55eedeea5af61f
ikemdinachi-alfred <ikemdinachialfred@gmail.com>
2024-11-18 01:56:33 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './component/home/HomePage';
 import LoginPage from './component/auth/LoginPage';
 import RegisterPage from './component/auth/RegisterPage';
 import Navbar from './component/common/Navbar';
+import FooterComponent from './component/common/Footer';
 
 function App() {
   return (
    <BrowserRouter>
    <div className='App'>
     <Navbar></Navbar>
+    <div className="content">
+    <FooterComponent></FooterComponent> 
    <Routes>
     {/*Public Routes */}
     <Route exact path='/home' element={<HomePage></HomePage>} />
     <Route exact path='/login' element={<LoginPage></LoginPage>} />
     <Route exact path='/register' element={<RegisterPage></RegisterPage>} />
 
+
   
-          
+    {/* Fallback Route */}
+    <Route path="*" element={<Navigate to="/login" />} />       
    </Routes>
+   </div>
+   
    </div>
    </BrowserRouter>
   );
---
+++ src/component/common/Footer.jsx
@@ -0,0 +1,14 @@
+const FooterComponent = () => {
+
+
+     return (
+         <footer>
+             <span className="my-footer">
+                 E - Rental Services | All Right Reserved &copy; {new Date().getFullYear()}
+             </span>
+         </footer>
+     );
+ };
+ 
+ export default FooterComponent;
+ 
